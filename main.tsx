950cbbe21f4598fcbb190404a5fa7d7d44f21a11
Pollyana Manuela Silva Rocha <pollyana.rocha@goop.com.br>
2025-11-12 20:09:48 -0300

    ajustando imports

--- main.tsx
+++ main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { TestAuthProvider } from './context/TestAuthContext'
+import './index.css'; // ou
+import './styles/index.css'; // se estiver em subpasta
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
