2eb4fe6a84878480d05e9db26afb67abc1965865
Pollyana Manuela Silva Rocha <pollyana.rocha@goop.com.br>
2025-11-12 20:18:13 -0300

    config

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '/src/index.css'
 import App from './App'
 import { TestAuthProvider } from './context/TestAuthContext'
 
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./"),
+      "@": path.resolve(__dirname, "./src"),
     },
     extensions: ['.tsx', '.ts', '.jsx', '.js']
   },
@@ -16,9 +16,6 @@ export default defineConfig({
   build: {
     outDir: "dist",
     emptyOutDir: true,
-    rollupOptions: {
-      external: []
-    }
   },
   base: './'
 });
