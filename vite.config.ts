10ad4baaaee46767d3e26fcc802753748fea3596
Pollyana Manuela Silva Rocha <pollyana.rocha@goop.com.br>
2025-11-13 07:20:32 -0300

    u

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  },
   server: {
+    port: 3000,
     proxy: {
-      '/api': 'http://localhost:3001'
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true
+      }
     }
   }
 })
